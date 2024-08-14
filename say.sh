4c1912c03f8ff529b890027ecfbd9f81e1810834
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-14 20:18:13 +0100

    Ignore dumb Pydantic crap

--- say.sh
+++ say.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PYTHONWARNINGS="ignore"
+
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 "$SCRIPT_DIR/.venv/bin/python" "$SCRIPT_DIR/src/main.py" "$@" || /usr/bin/say -v Samantha "$@"
